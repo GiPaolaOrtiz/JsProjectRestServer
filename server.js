1dc929b173ec659581ab5760a7d24990ab472cda
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2017-09-16 21:43:32 +0200

    arreglos

--- server.js
+++ server.js
@@ -491,7 +491,35 @@ app.post('/GuardarUsuario', (req, res) => {
     });
 });
 
+app.post('/subir', (req, res) => {
+    req.fields; // contains non-file fields 
+    req.files; // contains files 
+    var form = new formidable.IncomingForm();
+ 
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
 
+    });
+
+    form.on('end', function(fields, files) {
+        /* Temporary location of our uploaded file */
+        var temp_path = this.openedFiles[0].path;
+        /* The file name of the uploaded file */
+        var file_name = this.openedFiles[0].name;
+        /* Location where we want to copy the uploaded file */
+        var new_location = 'public/images/cuentos/';
+        fs.copy(temp_path, new_location + file_name, function(err) {  
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("success!")
+            }
+        });
+        res.end(file_name);
+    });
+    
+
+});
 
 
 console.log("Servidor iniciado");
